b81b24f908371c7394ecd325d8ad8a072f792616
99058830 <99058830@mydavinci.nl>
2021-12-14 18:46:40 +0100

    fixed typos

--- papigelato.py
+++ papigelato.py
@@ -1,56 +1,39 @@
 print('Welkom bij Papi Gelato, wij hebben de juiste smaken!')
 
+aardbei = 0
+chocolade = 0
+munt = 0
 vanille = 0
-chocolate = 0
-strawberry = 0
+
+slagroomPrijs = 0
+SprinklesPrijs = 0
+CaramelPrijs = 0
+
 aantalBol = 0
 aantalHoorn = 0
 aantalBak = 0
+
+totaalPrijs = 0
 hoornPrijs = 1.25
 bakPrijs = 0.75
 bolPrijs = 1.10
-toppingPrijs = 0.20
 
-def prijs():
-    global totaalPrijs, aantalBol, aantalHoorn, aantalBak, totaalbolPrijs, totaalbakPrijs, totaalhoornPrijs
-    totaalbolPrijs = float(aantalBol * bolPrijs)
-    totaalhoornPrijs = float(aantalHoorn * hoornPrijs)
-    totaalbakPrijs = float(aantalBak * bakPrijs)
-    totaalPrijs = float(totaalbolPrijs + totaalhoornPrijs + totaalbakPrijs)
-    print('------------------------')
-    if aantalBol >= 1: print(f'Bol      ' + str(aantalBol) + ' x ' + str(bolPrijs) + ' euro')
-    if aantalHoorn >= 1: print(f'Hoorn    ' + str(aantalHoorn) + ' x ' + str(hoornPrijs) + ' euro')
-    if aantalBak >= 1: print(f'Bak      ' + str(aantalBak) + ' x ' + str(bakPrijs) + ' euro')
-    print('smaak')
-    print('------------------------')
-    print(f'{totaalPrijs} euro')
-    afrekenen()
-    
-def afrekenen():
-    global totaalPrijs, aantalBol, aantalHoorn, aantalBak, totaalbolPrijs, totaalbakPrijs, totaalhoornPrijs
-    afrekenen = str(input('Wil je afrekenen? (J/N) >>> '))
-    if afrekenen == 'j':
-        print(f'Resterend bedrag om te betalen is {totaalPrijs} euro')
-        bedragbetalen = float(input('Typ het volledige bedrag in >>> '))
-        if bedragbetalen == totaalPrijs:
-            print('Bedankt voor de betaling, hier is uw ijs. Fijne dag verder')
-        else:
-            print('No money?')
-            (exit)
-    elif afrekenen == 'n':
-            afgifte = str(input('Wilt u nog meer bestellen? (J/N) >>> ')).lower()
-            if afgifte == 'j':
-                top()
-            elif afgifte == 'n':
-                afrekenen()
+def smaken():
+    print('AARDBEI - CHOCOLADE - MUNT - VANILLE')
+    smaak = str(input(f'Welke smaak wilt u? >>> ')).lower()
+    print(f'{smaak} dat is een lekkere smaak!')
+    smaakKeuze = str(input('Wil je nog een smaak kiezen? (J/N) >>> ')).lower()
+    if smaakKeuze == 'j':
+        smaken()
+    if smaakKeuze == 'n':
+        top()
     else:
-        print('Ik begrijp dit niet...')
-        exit()
+        sorry()
+        smaken()
 
 def top():
     global totaalPrijs, aantalBol, aantalBol2, aantalHoorn, aantalBak, totaalbolPrijs, totaalbakPrijs, totaalhoornPrijs
-    aantalBol = aantalBol2 = int(input('Hoeveel bolletjes wilt u? >>> '))
-    aantalBol + aantalBol2
+    aantalBol = int(input('Hoeveel bolletjes wilt u? >>> '))
     if aantalBol >= 1 and aantalBol <= 3:
         keuze = str(input(f'Wilt u deze {aantalBol} bolletje(s) in een hoorn of een bak? >>> ')).lower()
         if keuze == 'hoorn':
@@ -76,26 +59,52 @@ def top():
         sorry()
         top()
 
+def toppings():
+    global totaalPrijs, aantalBol, aantalHoorn, aantalBak, totaalbolPrijs, totaalbakPrijs, totaalhoornPrijs, topping
+    print('GEEN - SLAGROOM | €0,50 - SPINKLES €0,30 - CARAMEL SAUS')
+    topping = str(input(f'Welke smaak wilt u? >>>')).lower()
+
+def prijs():
+    global totaalPrijs, aantalBol, aantalHoorn, aantalBak, totaalbolPrijs, totaalbakPrijs, totaalhoornPrijs, topping
+    totaalbolPrijs = float(aantalBol * bolPrijs)
+    totaalhoornPrijs = float(aantalHoorn * hoornPrijs)
+    totaalbakPrijs = float(aantalBak * bakPrijs)
+    totaalslagroomPrijs = slagroomPrijs + 0.50
+    totaalsprinklesPrijs = aantalBol * 0.30
+    totaalcaramelPrijs = aantalBak * 0.90 and aantalHoorn * 0.60
+    totaalPrijs = float(totaalbolPrijs + totaalhoornPrijs + totaalbakPrijs)
+    print('------------------------')
+    if aantalBol >= 1: print(f'Bol      ' + str(aantalBol) + ' x ' + str(bolPrijs) + ' euro')
+    if aantalHoorn >= 1: print(f'Hoorn    ' + str(aantalHoorn) + ' x ' + str(hoornPrijs) + ' euro')
+    if aantalBak >= 1: print(f'Bak      ' + str(aantalBak) + ' x ' + str(bakPrijs) + ' euro')
+    if topping >= 1: print(f'Topping      ' + str(aantalBak) + ' x ' + str(bakPrijs) + ' euro')
+    print(smaak)
+    print('------------------------')
+    print(f'{totaalPrijs} euro')
+    afrekenen()
+    
+def afrekenen():
+    global totaalPrijs, aantalBol, aantalHoorn, aantalBak, totaalbolPrijs, totaalbakPrijs, totaalhoornPrijs
+    afrekenen = str(input('Wil je afrekenen? (J/N) >>> '))
+    if afrekenen == 'j':
+        print(f'Resterend bedrag om te betalen is {totaalPrijs} euro')
+        bedragbetalen = float(input('Typ het volledige bedrag in >>> '))
+        if bedragbetalen == totaalPrijs:
+            print('Bedankt voor de betaling, hier is uw ijs. Fijne dag verder')
+        else:
+            print('No money?')
+            (exit)
+    elif afrekenen == 'n':
+            afgifte = str(input('Wilt u nog meer bestellen? (J/N) >>> ')).lower()
+            if afgifte == 'j':
+                top()
+            elif afgifte == 'n':
+                afrekenen()
+    else:
+        print('Ik begrijp dit niet...')
+        exit()
+
 def sorry():
     print('Sorry, dat snap ik niet.')
-
-def smaken():
-    print('AARDBEI - CHOCOLADE - MUNT - VANILLE')
-    smaak = str(input(f'Welke smaak wilt u? >>> ')).lower()
-    print(f'{smaak} dat is een lekkere smaak!')
-    smaakKeuze = str(input('Wil je nog een smaak kiezen? (J/N) >>> ')).lower()
-    if smaakKeuze == 'j':
-        smaken()
-    if smaakKeuze == 'n':
-        top()
-    else:
-        sorry()
-        smaken()
-
-def toppings():
-    print('SPRINKLES - NOOTJES')
-    topping = str(input(f'Welke smaak wilt u? >>>'))
-    else:
-        sorry()
 
 smaken()
